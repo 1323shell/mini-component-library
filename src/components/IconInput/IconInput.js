109323281b305afb3f02765945ba031d774d8144
Yury Shelest <13shell@mail.ru>
2022-08-27 18:23:52 +0000

    Workshop: Mini Component Library

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
+
+const STYLES = {
+  small: {
+    fontSize: 14,
+    iconSize: 16,
+    borderThickness: 1,
+    height: 24,
+  },
+  large: {
+    fontSize: 18,
+    iconSize: 24,
+    borderThickness: 2,
+    height: 36,
+  },
+};
 
 const IconInput = ({
   label,
@@ -12,8 +27,63 @@ const IconInput = ({
   width = 250,
   size,
   placeholder,
+  ...props
 }) => {
-  return 'TODO';
+  const styles = STYLES[size];
+
+  return (
+    <Label>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <IconContainer style={{ "--size": styles.iconSize + "px" }}>
+        <Icon id="search" size={styles.iconSize} />
+      </IconContainer>
+      <TextInput
+        {...props}
+        style={{
+          "--width": width + "px",
+          "--height": styles.height / 16 + "rem",
+          "--border-thickness": styles.borderThickness + "px",
+          "--font-size": styles.fontSize / 16 + "rem",
+        }}
+      />
+    </Label>
+  );
 };
+
+const Label = styled.label`
+  display: block;
+  position: relative;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const TextInput = styled.input`
+  width: var(--width);
+  height: var(--height);
+  font-size: var(--font-size);
+  border: none;
+  border-bottom: var(--border-thickness) solid ${COLORS.black};
+  padding-left: var(--height);
+  color: inherit;
+  font-weight: 700;
+  outline-offset: 2px;
+
+  /* other styles omitted */
+  &::placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500};
+  }
+`;
+
+const IconContainer = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  height: var(--size);
+`;
 
 export default IconInput;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,69 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Container>
+      <SelectContainer value={value} onChange={onChange}>
+        {children}
+      </SelectContainer>
+      <Placeholder>
+        {displayedValue}
+        <IconContainer>
+          <Icon id="chevron-down" size={24} />
+        </IconContainer>
+      </Placeholder>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const SelectContainer = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`;
+
+const Placeholder = styled.div`
+  padding: 12px 16px;
+  padding-right: 52px;
+  border-radius: 8px;
+  font-size: ${16 / 16}rem;
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+
+  ${SelectContainer}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  ${SelectContainer}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconContainer = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+  margin: auto;
+  width: 24px;
+  height: 24px;
+  pointer-events: none;
+`;
 
 export default Select;
